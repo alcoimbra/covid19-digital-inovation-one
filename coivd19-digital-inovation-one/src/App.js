54887c19d94d17929bbf61b9a6ed524b9961aa42
Alan Coimbra <algcoimbra01@gmail.com>
2022-05-24 09:54:53 -0300

    Adicionando API no Projeto

--- coivd19-digital-inovation-one/src/App.js
+++ coivd19-digital-inovation-one/src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import { StylesProvider } from '@material-ui/styles'
 import { CssBaseline } from '@material-ui/core'
 import GlobalStyle from "./commons/styles/global-styled";
+import Main from "./containers/Main/index";
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
-      <div>
-        Teste
-      </div>
+      <Main />
     </StylesProvider>
   );
 }
